81b23b4a377e1716e09dcf99470d41ca7c3bbc3c
makenyw <1453397214@qq.com>
2016-12-29 10:19:00 +0800

    chapter6 add

--- countG.pl
+++ countG.pl
@@ -1,17 +1,19 @@
-#!/usr/bin/perl -w
+#! /usr/bin/perl -w
 use strict;
-my ($USAGE) ="$0 DNA\n\n";
-unless(@ARGV) {
-print $USAGE;
-exit;
+my($USAGE)= "$0 DNA\n\n";
+unless (@ARGV) {
+  print $USAGE;
+ exit;
 }
-my ($dna)=$ARGV[0];
-my ($num_of_Gs)= countG($dna);
-print"\n the DNA $dna has $num_of_Gs G\'s in it !\n\n";
+ my($dna)=$ARGV[0];
+my ($num_of_Gs)=countG($dna);
+print"\n The DNA $dna has $num_of_Gs  G\'s in it!\n\n";
 exit;
 sub countG {
 my($dna)=@_;
-my ($count)=0;
-$count =($dna =~ tr/Gg//);
+my($count)=0;
+$count=($dna =~ tr/Gg//);
 return $count;
 }
+
+
